97d2e8caabe95baa2b57111c256063bd25608665
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2024-04-23 22:14:19 +0700

    DatabaseFirst

---
+++ DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            using (var dbContext = new LAB_EF_CTYEntities())
+            {
+                var nhanViens = dbContext.NhanVien
+                    .Where(nv => nv.GioiTinh == "Nam" && nv.PhongBan.Ten == "Phòng Maketing" && nv.Tuoi >= 30 && nv.Tuoi <= 40)
+                    .ToList();
+
+                foreach (var nhanVien in nhanViens)
+                {
+                    Console.WriteLine($"Tên nhân viên: {nhanVien.Ten}, Tuổi: {nhanVien.Tuoi}, Phòng ban: {nhanVien.PhongBan.Ten}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
